f6766c0d977d9660cf47fee209cbeb48f690d17a
Mohamed <mhq187@gmail.com>
2018-01-28 18:06:33 +0000

    include libraries in .war build - little clean up

--- MovieProject/movie-web/src/main/java/com/mqul/mp/BaseController.java
+++ MovieProject/movie-web/src/main/java/com/mqul/mp/BaseController.java
@@ -21,26 +21,27 @@ public class BaseController
             "\t<head>\n" +
             "\t\t<title>projectMovie</title>\n" +
             "\n" +
-            "\t\t<meta name=\"author\" content=\"your name\" />\n" +
+            "\t\t<meta name=\"author\" content=\"mQulateen\" />\n" +
             "\t\t<meta name=\"description\" content=\"\" />\n" +
             "\n" +
             "\t\t<style>\n" +
             "\t\t\tp{ line-height: 1em; }\n" +
-            "\t\t\th1, h2, h3, h4{color: orange; font-weight: normal; line-height: 1.1em; margin: 0 0 .5em 0;}\n" +
+            "\t\t\th1, h2, h3, h4{color: #173e43; font-weight: normal; line-height: 1.1em; margin: 0 0 .5em 0;}\n" +
             "\t\t\th1{ font-size: 1.7em; }\n" +
             "\t\t\th2{ font-size: 1.5em; }\n" +
             "\t\t\ta{color: black; text-decoration: none;}\n" +
             "\t\t\ta:hover, a:active{ text-decoration: underline; }\n" +
-            "\t\t\tbody{font-family: arial; font-size: 80%; line-height: 1.2em; width: 100%; margin: 0; background: #eee;}\n" +
+            "\t\t\tbody{font-family: arial; font-size: 80%; line-height: 1.2em; width: 100%; margin: 0; background: #c9c9c9;}\n" +
             "\t\t\t#page{ margin: 20px; }\n" +
             "\t\t\t#logo{width: 35%; margin-top: 5px; font-family: georgia; display: inline-block;}\n" +
+            "\t\t\t#logo, h1 a {color:#173e43;}\n" +
             "\t\t\tnav{width: 60%; display: inline-block; text-align: right; float: right;}\n" +
             "\t\t\tnav ul{}\n" +
             "\t\t\tnav ul li{display: inline-block; height: 62px;}\n" +
-            "\t\t\tnav ul li a{padding: 20px; background: orange; color: white;}\n" +
-            "\t\t\tnav ul li a:hover{background-color: #ffb424; box-shadow: 0px 1px 1px #666;}\n" +
-            "\t\t\tnav ul li a:active{ background-color: #ff8f00; }\n" +
-            "\t\t\t#content{margin: 30px 0; background: white; padding: 20px; clear: both;}\n" +
+            "\t\t\tnav ul li a{padding: 20px; background: #173e43; color: white;}\n" +
+            "\t\t\tnav ul li a:hover{background-color: orange; box-shadow: 0px 1px 1px #666;}\n" +
+            "\t\t\tnav ul li a:active{ background-color: #3fb0ac; }\n" +
+            "\t\t\t#content{margin: 30px 0; background: #f6f1ed; padding: 20px; clear: both;}\n" +
             "\t\t\tfooter{border-bottom: 1px #ccc solid; margin-bottom: 10px;}\n" +
             "\t\t\tfooter p{text-align: right; text-transform: uppercase; font-size: 80%; color: grey;}\n" +
             "\t\t\t#content, ul li a{ box-shadow: 0px 1px 1px #999; }\n" +
@@ -55,10 +56,10 @@ public class BaseController
             "\n" +
             "\t\t\t<nav>\n" +
             "\t\t\t\t<ul>\n" +
-            "\t\t\t\t\t<li><a href=\"/pmovie\">Home</a></li>\n" +
-            "\t\t\t\t\t<li><a href=\"/pmovie/film\">Films</a></li>\n" +
-            "\t\t\t\t\t<li><a href=\"/pmovie/actor\">Actors</a></li>\n" +
-            "\t\t\t\t\t<li><a href=\"/pmovie/director\">Directors</a></li>\n" +
+            "\t\t\t\t\t<li><a href=\"/\">Home</a></li>\n" +
+            "\t\t\t\t\t<li><a href=\"/film\">Films</a></li>\n" +
+            "\t\t\t\t\t<li><a href=\"/actor\">Actors</a></li>\n" +
+            "\t\t\t\t\t<li><a href=\"/director\">Directors</a></li>\n" +
             "\t\t\t\t</ul>\t\n" +
             "\t\t\t</nav>\n" +
             "\n" +
